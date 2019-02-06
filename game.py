da4000762aa73321929028d9acb9ee6d02836abe
Lizband <trash.block2@gmail.com>
2019-02-06 18:13:13 -0500

    Change object images to sprites

--- game.py
+++ game.py
@@ -34,7 +34,7 @@ clock = pygame.time.Clock()
 framerate = 60
 
 # Load sprites
-fox = pygame.sprite.Sprite()  # create sprite
+fox = pygame.sprite.Sprite()  # create fox sprite
 fox.base_image = pygame.image.load('sprites/entities/fox/fox_base.png').convert()
 fox.right = pygame.transform.scale2x(fox.base_image)
 fox.left = pygame.transform.flip(fox.right, True, False)
@@ -42,10 +42,15 @@ fox.image = fox.right
 fox.rect = fox.image.get_rect()  # use image extent values
 fox.width, fox.height = fox.rect.size
 
-grass = pygame.image.load('sprites/environment/forest/grass.png')
-dirt = pygame.image.load('sprites/environment/forest/dirt.png')
+grass = pygame.sprite.Sprite()  # create grass sprite
+grass.image = pygame.image.load('sprites/environment/forest/grass.png')
+dirt = pygame.sprite.Sprite()  # create dirt sprite
+dirt.image = pygame.image.load('sprites/environment/forest/dirt.png')
+stone = pygame.sprite.Sprite()  # create stone sprite
 stone = pygame.image.load('sprites/environment/forest/stone.png')
+tree = pygame.sprite.Sprite()  # create tree sprite
 tree = pygame.image.load('sprites/environment/forest/tree.png')
+box = pygame.sprite.Sprite()  # create box sprite
 box = pygame.image.load('sprites/environment/other/box.png')
 
 
@@ -55,90 +60,88 @@ def player(img, pos):
 
 
 # Temp map
-# Grass
 def grid(pos, x_size=32, y_size=32):
     x, y = pos
     return x * x_size, y * y_size
 
-
-map_1 = {
-    # Left wall
-    dirt: [
-        (0, 0),
-        (0, 1),
-        (0, 2),
-        (0, 3),
-        (0, 4),
-        (0, 5),
-        (0, 6),
-        (0, 7),
-        (0, 8),
-        (0, 9),
-        (0, 10),
-        (0, 11),
-        (0, 12),
-        (0, 13),
-        (0, 14),
-        (0, 15),
-        (0, 16),
-        (0, 17),
-        (0, 18),
-        # Right wall
-        (24, 0),
-        (24, 1),
-        (24, 2),
-        (24, 3),
-        (24, 4),
-        (24, 5),
-        (24, 6),
-        (24, 7),
-        (24, 8),
-        (24, 9),
-        (24, 10),
-        (24, 11),
-        (24, 12),
-        (24, 13),
-        (24, 14),
-        (24, 15),
-        (24, 16),
-        (24, 17),
-        (24, 18),
-        ],
-    grass: [
-        # floor
-        (1, 18),
-        (2, 18),
-        (3, 18),
-        (4, 18),
-        (5, 18),
-        (6, 18),
-        (7, 18),
-        (8, 18),
-        (9, 18),
-        (10, 18),
-        (11, 18),
-        (12, 18),
-        (13, 18),
-        (14, 18),
-        (15, 18),
-        (16, 18),
-        (17, 18),
-        (18, 18),
-        (19, 18),
-        (20, 18),
-        (21, 18),
-        (22, 18),
-        (23, 18),
-        ],
-    stone: [
-        (18, 17),
-        (18, 16),
-        (17, 17)],
-    box: [
-        (18,153)
-
-    ]
-}
+# map_1 = {
+#     # Left wall
+#     dirt: [
+#         (0, 0),
+#         (0, 1),
+#         (0, 2),
+#         (0, 3),
+#         (0, 4),
+#         (0, 5),
+#         (0, 6),
+#         (0, 7),
+#         (0, 8),
+#         (0, 9),
+#         (0, 10),
+#         (0, 11),
+#         (0, 12),
+#         (0, 13),
+#         (0, 14),
+#         (0, 15),
+#         (0, 16),
+#         (0, 17),
+#         (0, 18),
+#         # Right wall
+#         (24, 0),
+#         (24, 1),
+#         (24, 2),
+#         (24, 3),
+#         (24, 4),
+#         (24, 5),
+#         (24, 6),
+#         (24, 7),
+#         (24, 8),
+#         (24, 9),
+#         (24, 10),
+#         (24, 11),
+#         (24, 12),
+#         (24, 13),
+#         (24, 14),
+#         (24, 15),
+#         (24, 16),
+#         (24, 17),
+#         (24, 18),
+#         ],
+#     grass: [
+#         # floor
+#         (1, 18),
+#         (2, 18),
+#         (3, 18),
+#         (4, 18),
+#         (5, 18),
+#         (6, 18),
+#         (7, 18),
+#         (8, 18),
+#         (9, 18),
+#         (10, 18),
+#         (11, 18),
+#         (12, 18),
+#         (13, 18),
+#         (14, 18),
+#         (15, 18),
+#         (16, 18),
+#         (17, 18),
+#         (18, 18),
+#         (19, 18),
+#         (20, 18),
+#         (21, 18),
+#         (22, 18),
+#         (23, 18),
+#         ],
+#     stone: [
+#         (18, 17),
+#         (18, 16),
+#         (17, 17)],
+#     box: [
+#         (18,153)
+#
+#     ]
+# }
 
 
 def display_image(img, pos):
@@ -198,7 +201,7 @@ while not game_exit:
 
     game_display.fill(sky_blue)
     player(fox.image, (x, y))
-    display_map(map_1)
+    display_map()
     pygame.display.update()
     clock.tick(framerate)
 
