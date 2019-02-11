6b54beb28556077058c5cb33d42985a792b7a759
Lizband <trash.block2@gmail.com>
2019-02-11 07:43:18 -0500

    Change levels to ASCII levels

--- game/levels/level_1.py
+++ game/levels/level_1.py
@@ -1,36 +1,4 @@
-from game.models import Grass, Stone, Dirt, Cloud
+from game.levels.loader import ascii_map
 
-terrain = [
-    # Floor (cover)
-    Grass(0, 18),
-    Grass(1, 18),
-    Grass(2, 18),
-    Grass(3, 18),
-    Grass(4, 18),
-    Grass(5, 18),
-    Grass(6, 18),
-    Grass(7, 18),
-    Grass(8, 18),
-    Grass(9, 18),
-    Grass(10, 18),
-    Grass(11, 18),
-    Dirt(12, 18),
-    Grass(13, 18),
-    Grass(14, 18),
-    Grass(15, 18),
-    Grass(16, 18),
-    Grass(17, 18),
-    Grass(18, 18),
-    Grass(19, 18),
-    Grass(20, 18),
-    Grass(21, 18),
-    Grass(22, 18),
-    Grass(23, 18),
-    Grass(24, 18),
 
-    # Inside
-    Dirt(12, 17),
-    Grass(12, 16),
-    Cloud(0, 0),
-
-]
+terrain = ascii_map('game/levels/level_1.map')
--- game/levels/level_3.py
+++
@@ -1,4 +0,0 @@
-from game.levels.loader import ascii_map
-
-
-terrain = ascii_map('game/levels/template.map')
